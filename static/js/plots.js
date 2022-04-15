9670cc4fb8be0106c526bf5f6965fff5e7b0f6bb
Melissa Raines <melissajoyraines@gmail.com>
2022-04-14 20:46:28 -0400

    Update plots.js

--- static/js/plots.js
+++ static/js/plots.js
@@ -1,8 +1,9 @@
-const url = “http://127.0.0.1:5000/get-data”;
+const url = "http://127.0.0.1:5000/get-data";
+
 // Fetch the JSON data and console log it
 d3.json(url).then(function(data) {
   // console.log(data);
   for (let i = 0; i < data.length; i++) {
-    let yAxis = (data[i][“Mx”]);
+    let yAxis = (data[i]["Mx"]);
   }
 });
