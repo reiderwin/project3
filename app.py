aa7247c87ed48a522f804694c643904808bb795a
Melissa Raines <melissajoyraines@gmail.com>
2022-04-14 20:43:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,18 +7,18 @@ import json
 from bson import ObjectId
 app = Flask(__name__)
 CORS(app)
-app.config[“MONGO_URI”] = “mongodb://localhost:27017/states_db”
+app.config["MONGO_URI"] = "mongodb://localhost:27017/states_db"
 mongo = PyMongo(app)
-@app.route(“/”)
+@app.route("/")
 def index():
-    return render_template(“index.html”)
-@app.route(“/get-data”)
+    return render_template("index.html")
+@app.route("/get-data")
 def get_data():
     cancer_data = mongo.db.cancer_data
     output = []
     for q in cancer_data.find():
-        output.append({‘Index’: q[‘Index’], ‘Location’ : q[‘Location’], ‘Sex’ : q[‘Sex’], ‘Year’ : q[‘Year’], ‘Mx’ : q[‘Mx’], ‘Upper’ : q[‘Upper’], ‘Lower’ : q[‘Lower’]})
+        output.append({'Index': q['Index'], 'Location' : q['Location'], 'Sex' : q['Sex'], 'Year' : q['Year'], 'Mx' : q['Mx'], 'Upper' : q['Upper'], 'Lower' : q['Lower']})
     return jsonify(output)
-if __name__ == ‘__main__‘:
+if __name__ == '__main__':
     app.debug = True
     app.run()
