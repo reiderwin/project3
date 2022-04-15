409b2c27ca400ce4b2f96b72e0416dc193348213
Melissa Raines <melissajoyraines@gmail.com>
2022-04-14 20:30:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,33 +5,20 @@ from flask_cors import CORS
 from bson import json_util
 import json
 from bson import ObjectId
-
-
 app = Flask(__name__)
 CORS(app)
-
-app.config["MONGO_URI"] = "mongodb://localhost:27017/states_db"
+app.config[“MONGO_URI”] = “mongodb://localhost:27017/states_db”
 mongo = PyMongo(app)
-
-
-
-@app.route("/")
+@app.route(“/”)
 def index():
-    return render_template("index.html")
-
-
-@app.route("/get-data")
+    return render_template(“index.html”)
+@app.route(“/get-data”)
 def get_data():
     cancer_data = mongo.db.cancer_data
-
     output = []
     for q in cancer_data.find():
-        output.append({'Index': q['Index'], 'Location' : q['Location'], 'Sex' : q['Sex'], 'Year' : q['Year'], 'Mx' : q['Mx'], 'Upper' : q['Upper'], 'Lower' : q['Lower']})
-    return jsonify({'RESULT' : output})
-
-if __name__ == '__main__':
+        output.append({‘Index’: q[‘Index’], ‘Location’ : q[‘Location’], ‘Sex’ : q[‘Sex’], ‘Year’ : q[‘Year’], ‘Mx’ : q[‘Mx’], ‘Upper’ : q[‘Upper’], ‘Lower’ : q[‘Lower’]})
+    return jsonify(output)
+if __name__ == ‘__main__‘:
     app.debug = True
     app.run()
-
-
-
